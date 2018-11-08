30c1d868bf7a50778cb6c718f5eda2671efac18e
Douglas Barbosa <douglas.ba182@gmail.com>
2018-11-08 20:49:29 -0200

    Trabalho 6

---
+++ src/main/java/persistence/AlunoDAO.java
@@ -0,0 +1,17 @@
+package persistence;
+
+import java.util.List;
+
+import br.com.prog3.trabalho6java.model.Aluno;
+
+public interface AlunoDAO {
+	public String save(Aluno aluno);
+
+	public String delete(int matricula);
+
+	public String update(Aluno aluno);
+
+	public List<Aluno> list();
+
+	public Aluno findByMatricula(int matricula);
+}
